87e923a3bfe390cd6c9e3fcc1d3e5fbad8869776
Jason Song <wolfogre@outlook.com>
2015-12-28 23:04:11 +0800

    Splited  *. h into *.h & *.cpp

--- Exp6/Game.h
+++ Exp6/Game.h
@@ -1,105 +1,17 @@
 #pragma once
 
-#include <iostream>
-#include <ctime>
 #include "Player.h"
 
 class Game
 {
 public:
-	Game()
-	{
-		srand(time(0)); //seed the random number generator
-		player = new Player(50, 25, 100);
-		germania = new Player(rand() % (51) + 20, rand() % (41) + 10, rand() % (101) + 50);
-		germania->SetName("Germania");
-	}
-	~Game()
-	{
-		delete player;
-		delete germania;
-	}
-	void Start(){
-		Welcome();
-		while (!end) //main game loop
-		{
-			player->SetSend(0, 0, 0);
-			player->ShowStatus();
-			germania->ShowStatus();
-			
-			while (Menu())
-				;
-
-			player->BattleWith(end, lost, *germania);
-		} //end of main game loop
-
-		//display appropriate ending message
-		GameOver();
-	}
+	Game();
+	~Game();
+	void Start();
 protected:
-	void Welcome()
-	{
-		std::cout << "Welcome Adventurer, what is your name?\n";
-		player->SetName();
-		std::cout << "Well, " << player->GetName()
-			<< " welcome to the Roman Commander Game.\n"
-			<< "\nYou are the commander of the Roman Army"
-			<< " attacking Germania.";
-	}
-	bool Menu()
-	{
-		int menu_choice; //stores the user¡¯s choice from the menu
-		//stores which numbers correspond to which menu choices
-		int archers_menu, catapults_menu, swordsmen_menu;
-		int fight_menu;
-
-		//keeps track of which menu numbers
-		//are being used
-		int i = 1;
-		if (player->GetRemainSolders(Player::ARCHER) > 0)
-		{
-			archers_menu = i;
-			std::cout << "[" << i << "] Send Archers\n";
-			i++;
-		}
-		else archers_menu = 0;
-		if (player->GetRemainSolders(Player::CATAPULT) > 0)
-		{
-			catapults_menu = i;
-			std::cout << "[" << i << "] Send Catapults\n";
-			i++;
-		}
-		else catapults_menu = 0;
-		if (player->GetRemainSolders(Player::SWORDSMAN) > 0)
-		{
-			swordsmen_menu = i;
-			std::cout << "[" << i << "] Send Swordsmen\n";
-			i++;
-		}
-		else swordsmen_menu = 0;
-		fight_menu = i;
-		std::cout << "[" << i << "] Go Fight\n";
-
-		std::cin >> menu_choice;
-		if (menu_choice == archers_menu)
-			player->SetSend(Player::ARCHER);
-		else if (menu_choice == catapults_menu)
-			player->SetSend(Player::CATAPULT);
-		else if (menu_choice == swordsmen_menu)
-			player->SetSend(Player::SWORDSMAN);
-		else if (menu_choice == fight_menu)
-			return false;
-		return true;
-	}
-	void GameOver()
-	{
-		if (lost)
-		{
-			std::cout << "\nYou lost. Try again next time.\n";
-			return;
-		}
-		std::cout << "\nCongratulations, you won!\n";
-	}
+	void Welcome();
+	bool Menu();
+	void GameOver();
 private:
 	Player *player;
 	Player *germania;
--- Exp6/Player.h
+++ Exp6/Player.h
@@ -7,141 +7,16 @@ class Player
 {
 public:
 	enum SolderType { ARCHER, CATAPULT, SWORDSMAN };
-	Player(int archers, int catapults, int swordsmen)
-	{
-		this->archers = archers;
-		this->catapults = catapults;
-		this->swordsmen = swordsmen;
-	}
-	~Player()
-	{
-
-	}
-	void SetName()
-	{
-		std::cin >> name;
-	}
-	void SetName(std::string name)
-	{
-		this->name = name;
-	}
-	std::string GetName()
-	{
-		return name;
-	}
-	void ShowStatus()
-	{
-		std::cout << "\n" << name << " has " << archers
-			<< " archers, " << catapults
-			<< " catapults, and "
-			<< swordsmen << " swordsmen.\n";
-	}
-	int GetRemainSolders(SolderType st)
-	{
-		switch (st)
-		{
-		case Player::ARCHER:
-			return archers - archers_sent;
-			break;
-		case Player::CATAPULT:
-			return catapults - catapults_sent;
-			break;
-		case Player::SWORDSMAN:
-			return swordsmen - swordsmen_sent;
-			break;
-		}
-	}
-	void SetSend(SolderType st)
-	{
-		switch (st)
-		{
-		case Player::ARCHER:
-			do {
-				std::cout << "How many archers"
-					" would you like to send?\n";
-				std::cin >> archers_sent;
-			} while (!(archers_sent > -1
-				&& archers_sent <= archers));
-			break;
-		case Player::CATAPULT:
-			do {
-				std::cout << "How many catapults"
-					" would you like to send?\n";
-				std::cin >> catapults_sent;
-			} while (!(catapults_sent > -1 &&
-				catapults_sent <= catapults));
-			break;
-		case Player::SWORDSMAN:
-			do {
-				std::cout << "How many swordsmen"
-					" would you like to send?\n";
-				std::cin >> swordsmen_sent;
-			} while (!(swordsmen_sent > -1 &&
-				swordsmen_sent <= swordsmen));
-			break;
-		}
-	}
-	void SetSend(int archers_sent, int catapults_sent, int swordsmen_sent)
-	{
-		this->archers_sent = archers_sent;
-		this->catapults_sent = catapults_sent;
-		this->swordsmen_sent = swordsmen_sent;
-	}
-	void BattleWith(bool & end, bool & lost, Player & other_player)
-	{
-		std::cout << "\nEntering Battle...\n";
-
-		int archers_dead, catapults_dead, swordsmen_dead;
-		int g_archers_dead, g_catapults_dead;
-		int g_swordsmen_dead;
-
-		//each catapult kills 2 archers
-		archers_dead = 2 * other_player.catapults;
-		//each swordsman kills 1 catapult
-		catapults_dead = other_player.swordsmen;
-		//each archer kills 3 swordsmen
-		swordsmen_dead = 3 * other_player.archers;
-
-		g_archers_dead = 2 * catapults_sent;
-		g_catapults_dead = swordsmen_sent;
-		g_swordsmen_dead = 3 * archers_sent;
-
-		//makes sure that the number of 
-		//units does not go below 0.
-		archers = (archers_dead < archers) ?
-			archers - archers_dead : 0;
-		catapults = (catapults_dead < catapults) ?
-			catapults - catapults_dead : 0;
-		swordsmen = (swordsmen_dead < swordsmen) ?
-			swordsmen - swordsmen_dead : 0;
-
-		other_player.archers = (g_archers_dead < other_player.archers) ?
-			other_player.archers - g_archers_dead : 0;
-		other_player.catapults = (g_catapults_dead < other_player.catapults) ?
-			other_player.catapults - g_catapults_dead : 0;
-		other_player.swordsmen = (g_swordsmen_dead < other_player.swordsmen) ?
-			other_player.swordsmen - g_swordsmen_dead : 0;
-
-		std::cout << "It was a long battle. "
-			<< archers_dead << " archers died.\n"
-			<< catapults_dead << " catapults died.\n"
-			<< swordsmen_dead << " swordsmen died.\n";
-		//if player¡¯s army is dead than they have lost
-		if ((archers + catapults + swordsmen) == 0)
-		{
-			end = lost = true;
-			return;
-		}
-		//if germanium army is dead, player has won
-		if ((other_player.archers + other_player.catapults
-			+ other_player.swordsmen) == 0)
-		{
-			lost = false;
-			end =  true;
-			
-		}
-		end = false;
-	}
+	Player(int archers, int catapults, int swordsmen);
+	~Player();
+	void SetName();
+	void SetName(std::string name);
+	std::string GetName();
+	void ShowStatus();
+	int GetRemainSolders(SolderType st);
+	void SetSend(SolderType st);
+	void SetSend(int archers_sent, int catapults_sent, int swordsmen_sent);
+	void BattleWith(bool & end, bool & lost, Player & other_player);
 private:
 	std::string name;; //used to store the player¡¯s name
 
